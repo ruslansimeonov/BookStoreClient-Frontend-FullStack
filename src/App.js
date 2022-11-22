a538cf0f363d1fc2d112cf351e26c45ccc75f910
Ruslan Simeonov <ruslansimenov@gmail.com>
2022-11-22 15:19:00 +0100

    Setup and connect to backend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React from "react";
+import baseUrl from "./config";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  axios(`${baseUrl}/api/v1/books`).then((books) => {
+    console.log(books);
+  });
+
+  return <div>My first component</div>;
+};
 
 export default App;
